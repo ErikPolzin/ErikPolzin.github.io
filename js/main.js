70fe3a44f9ac0aadb7b7c6907151bf535bf2281b
ErikPolzin <eriktpol@gmail.com>
2018-08-25 10:44:54 +0200

    added about blurb

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
   M.AutoInit();
+  let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  if (windowWidth < 1000) {
+    document.addEventListener('scroll', minimiseHeader);
+  }
 });
+
+let lastScrollTop = 0;
+function minimiseHeader() {
+  // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+  let st = window.pageYOffset || document.documentElement.scrollTop;
+  if (st > lastScrollTop){
+    // downscroll code
+    document.getElementById('site-title').style.display = "none";
+  } else {
+    // upscroll code
+    document.getElementById('site-title').style.display = "block";
+  }
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}
